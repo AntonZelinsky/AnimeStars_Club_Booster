394ea22cc50160fd2c5d2846458b0bd117ae58de
Anton Zelinsky <Anton.Zelinsky@gmail.com>
2025-06-17 23:57:22 +0200

    Upgrade boost handler and add README file

--- script.user.js
+++ script.user.js
@@ -3,18 +3,15 @@
 // @name:en         AnimeStars Club Booster
 // @name:ru         AnimeStars Club Booster
 // @namespace       http://tampermonkey.net/
-// @version         2025-06-07.3
+// @version         2025-06-17
 // @description     Скрипт для автоматизации внесения вкладов карт в клубы AnimeStars.org
 // @description:ru  Скрипт для автоматизации внесения вкладов карт в клубы AnimeStars.org
 // @description:en  The script for automating card boosting in clubs AnimeStars.org
 // @author          Anton Zelinsky
 // @match           https://animestars.org/clubs/boost/?id=*
-// @match           https://astars.club/clubs/boost/?id=*
-// @match           https://asstars.club/clubs/boost/?id=*
-// @match           https://asstars1.astars.club/clubs/boost/?id=*
-// @match           https://as1.astars.club/clubs/boost/?id=*
 // @match           https://asstars.tv/clubs/boost/?id=*
-// @match           https://ass.astars.club/clubs/boost/?id=*
+// @match           https://astars.club/clubs/boost/?id=*
+// @match           https://*.astars.club/clubs/boost/?id=*
 // @run-at          document-idle
 // @license         MIT
 // @icon            https://www.google.com/s2/favicons?sz=64&domain=animestars.org
@@ -25,7 +22,7 @@
 // ==/UserScript==
 
 
-const DELAY_MS = 1000; // 1 секунда = 1000 миллисекунд
+const DELAY_SEC = 1.5;
 
 (function () {
   "use strict"
@@ -50,80 +47,79 @@ const DELAY_MS = 1000; // 1 секунда = 1000 миллисекунд
     return diffMs > 0 ? Math.floor(diffMs / 1000) : 0
   }
 
-  function isTimeExpired() {
-    const moscowTime = getMoscowTime()
-    if (moscowTime.getHours() > 22) {
-      console.info('🏁 Время пожертвования карт закончилось.')
-      return true;
-    }
-    return false;
-  }
-
   function sleep(seconds) {
-    return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+    return new Promise(resolve => setTimeout(() => resolve(true), seconds * 1000));
   }
 
   function reloadPageAfter5min() {
-    // Необходима перезагрузка страницы, так как счётчик внесённых карт автоматически не обновляется
-    console.log(`Страница перезагрузится через 5 минут.`)
+    // Перезагрузка страницы, для обновления общей статистики вкладов
+    console.log('Страница перезагрузится через 5 минут.')
     setTimeout(() => {
       location.reload();
     }, 5 * 60 * 1000); // 5 минут
   }
 
   function isBoostLimitReached() {
-    const limitCounter = document.querySelector(".boost-limit").innerText
-    return limitCounter == 300
+    const limitCounter = document.querySelector('.boost-limit').innerText
+    if (limitCounter == 300) {
+      console.info(`💳 Лимит карт исчерпан: ${new Date().toLocaleTimeString()}.`)
+      return true
+    }
+    return false
   }
 
-  function handleBoost() {
-    console.log(`Внесение вкладов начато.`)
-    const intervalId = setInterval(() => {
-      const refreshBtn = document.querySelector(".button.button--primary.club__boost__refresh-btn")
+  function formatTimeLeft(seconds) {
+    const hrs = Math.floor(seconds / 3600)
+    const mins = Math.floor((seconds % 3600) / 60)
+    const secs = seconds % 60
+
+    const parts = []
+    if (hrs > 0) parts.push(`${hrs} ч`)
+    if (mins > 0) parts.push(`${mins} мин`)
+    if (secs > 0 || parts.length === 0) parts.push(`${secs} сек`)
+    
+    return parts.join(' ')
+  }
+
+  async function handleBoost() {
+    console.log('Внесение вкладов начато.')
+    do {
+      const refreshBtn = document.querySelector('.button.button--primary.club__boost__refresh-btn')
       if (refreshBtn) {
         refreshBtn.click()
-        console.log(`🌀 Обновлена карта.`)
-        return
+        console.log('🌀 Обновлена карта.')
+        await sleep(0.2)
       }
 
-      const contributeBtn = document.querySelector(".button.button--primary.club__boost-btn")
+      const contributeBtn = document.querySelector('.button.button--primary.club__boost-btn')
       if (contributeBtn) {
         contributeBtn.click()
         console.info(`💳 Внесена карта: ${new Date().toLocaleTimeString()}.`)
+        await sleep(DELAY_SEC)
+      }
+
+      if (isBoostLimitReached()) {
         return
       }
 
-      if (isTimeExpired(intervalId)) {
-        clearInterval(intervalId);
-        return
-      }
-
-      console.log("⏳ Кнопки не найдены, жду...", new Date().toLocaleTimeString())
-    }, DELAY_MS);
+    } while(await sleep(DELAY_SEC))
   }
 
   async function run() {
-    console.log("Начало работы ...")
+    console.log('Начало работы скрипта автовкладов...')
+
     reloadPageAfter5min()
 
     const secondsLeft = getSecondsUntil2101Moscow()
-    if (secondsLeft > 0) {
-      console.log(`До 21:01 Мск осталось ${secondsLeft} секунд.`)
-      await sleep(secondsLeft + 1)
+    if (isBoostLimitReached() && secondsLeft > 0) {
+      console.log(`До 21:01 Мск осталось ${formatTimeLeft(secondsLeft)}.`)
+      await sleep(secondsLeft)
       location.reload()
       return
     }
 
-    if(isBoostLimitReached()) {
-      console.info(`💳 Лимит карт исчерпан: ${new Date().toLocaleTimeString()}.`)
-      return
-    }
-    if (isTimeExpired()) {
-      return
-    }
-
-    handleBoost()
-    console.log(`🏁 Внесение вкладов завершено.`)
+    await handleBoost()
+    console.log('🏁 Внесение вкладов завершено.')
   }
 
   run()
